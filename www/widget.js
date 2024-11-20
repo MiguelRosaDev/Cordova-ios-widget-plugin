e99279b9099011564024712879d15965845d0318
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-11-20 09:56:12 +0000

    Update widget.js

--- www/widget.js
+++ www/widget.js
@@ -2,7 +2,7 @@ var exec = require('cordova/exec');
 
 var iOSWidget = {
     updateWidget: function(text, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "AndroidWidget", "updateWidget", [text]);
+        exec(successCallback, errorCallback, "iOSWidget", "updateWidget", [text]);
         }
     }
 };
